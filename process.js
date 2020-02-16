62565a525ffa0ac804dfec5c4336941c468b26d5
Rishub Podar <rishubpodar@wirelessprv-10-193-30-10.near.illinois.edu>
2020-02-15 19:41:32 -0600

    Started working on ajax

--- process.js
+++ process.js
@@ -160,3 +160,20 @@ module.exports = {
     toLookupJson,
     zipcodeCsvToJson
 }
+
+$(document).ready(function() {
+    $('form').on('submit', function(event) {
+      $.ajax({
+         data : {
+            firstName : $('#firstName').val(),
+            lastName: $('#lastName').val(),
+                },
+            type : 'POST',
+            url : '/process'
+           })
+       .done(function(data) {
+         $('#output').text(data.output).show();
+     });
+     event.preventDefault();
+     });
+});
